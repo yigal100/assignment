9774df946bbc810d93477163930f982824e3036b
Yigal Chripun <yigal.chripun@gmail.com>
2017-11-26 01:53:14 +0200

    rename project

--- WebApi/Controllers/RecordsController.cs
+++ WebApi/Controllers/RecordsController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace assignment.Controllers
 {
     [Route("api/[controller]")]
-    public class ValuesController : Controller
+    public class RecordsController : Controller
     {
         // GET api/values
         [HttpGet]
---
+++ WebApi/DTOs/records.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace assignment.DTOs
+{
+    public class RecordDto
+    {
+    }
+
+    public class VariableDto { }
+}
