4791ac99b910fa885259c48a13b074d19aebca94
Yigal Chripun <yigal.chripun@gmail.com>
2017-12-10 07:03:32 +0200

    docker again

--- Angular/src/app/google-chart.directive.spec.ts
+++ Angular/src/app/google-chart.directive.spec.ts
@@ -1,8 +1,11 @@
 import { GoogleChartDirective } from './google-chart.directive';
+import { ElementRef } from '@angular/core';
 
 describe('GoogleChartDirective', () => {
   it('should create an instance', () => {
-    const directive = new GoogleChartDirective();
+    let element;
+    let elem = new ElementRef(element);
+    const directive = new GoogleChartDirective(elem);
     expect(directive).toBeTruthy();
   });
 });
